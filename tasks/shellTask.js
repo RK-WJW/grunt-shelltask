8a4b06a77d616cd4bba4f43e120ed0d7ae3ac907
RK-WJW <javawjw@163.com>
2014-04-22 15:33:14 +0800

    update

--- tasks/shellTask.js
+++ tasks/shellTask.js
@@ -20,24 +20,52 @@ module.exports = function (grunt) {
         var options = this.options({
                 logger: logger
             });
+        //用于外部使用提供的对象
+        var obj = {
+            "options" : options,
+            "task" : task,
+            "getResult": function (id){
+                id = id || 0;
+                if(typeof id === 'number'){
+                    return task[id].ret;
+                }
+                for(var i = 0; i < task.length; i++){
+                    var item = task[i];
+                    if(id == item.id){
+                        return item.ret;
+                    }
+                }
+            },
+            "prev": null
+        };
         //按顺序执行
         async.mapSeries(task, function (item, callback) {
             var remote = item.remote;
             var workPath = options.localWorkPath;
             var command = item.command;
+            var after = item.after;
+            
             var _cb = (function (item) {
                 return function (err, data) {
-                    item.ret = err || data;
-                    callback(err, data);
+                    var ret = data;
+                    if(err){
+                        item.error = err;
+                        callback(err, data);
+                    }
+                    if(typeof after === 'function'){
+                        ret = after.apply(obj, [data]);
+                    }
+                    item.ret = ret;
+                    obj.prev = item;
+                    callback(null, ret);
                 };
             })(item);
 
             if (typeof command === 'function') {
-                command = command.apply({
-                        "options" : options,
-                        "task" : task
-                    });
+                command = command.apply(obj, [obj.prev]);
             }
+            command = command.replace(/\[(%=\s?[^%]*\s?%)\]/g, "<$1>");
+            command = grunt.template.process(command, {data: options});
             //命令为exit时终止任务
             if(command === "exit"){
                 _cb("exit");
