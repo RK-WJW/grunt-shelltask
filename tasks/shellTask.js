8b4159bbfd935fefdc52a263fbbf97fc214c401a
RK-WJW <javawjw@163.com>
2014-04-23 17:17:06 +0800

    update

--- tasks/shellTask.js
+++ tasks/shellTask.js
@@ -6,12 +6,8 @@
  */
 
 'use strict';
-var path = require("path");
-var async = require('async');
-var ssh = require('./lib/ssh.js');
-var localCmd = require('./lib/localCmd.js');
-var logger = console;
-var sshArray = [];
+var sheller = require('./lib/sheller.js');
+var logger = sheller.logger;
 
 module.exports = function (grunt) {
     grunt.registerMultiTask('shelltask', 'grunt exec shell task', function () {
@@ -20,82 +16,10 @@ module.exports = function (grunt) {
         var options = this.options({
                 logger: logger
             });
-        //用于外部使用提供的对象
-        var obj = {
-            "options" : options,
-            "task" : task,
-            "getResult": function (id){
-                id = id || 0;
-                if(typeof id === 'number'){
-                    return task[id].ret;
-                }
-                for(var i = 0; i < task.length; i++){
-                    var item = task[i];
-                    if(id == item.id){
-                        return item.ret;
-                    }
-                }
-            },
-            "prev": null
-        };
-        //按顺序执行
-        async.mapSeries(task, function (item, callback) {
-            var remote = item.remote;
-            var workPath = options.localWorkPath;
-            var command = item.command;
-            var after = item.after;
-            
-            var _cb = (function (item) {
-                return function (err, data) {
-                    var ret = data;
-                    if(err){
-                        item.error = err;
-                        callback(err, data);
-                    }
-                    if(typeof after === 'function'){
-                        ret = after.apply(obj, [data]);
-                    }
-                    item.ret = ret;
-                    obj.prev = item;
-                    callback(null, ret);
-                };
-            })(item);
-
-            if (typeof command === 'function') {
-                command = command.apply(obj, [obj.prev]);
-            }
-            command = command.replace(/\[(%=\s?[^%]*\s?%)\]/g, "<$1>");
-            command = grunt.template.process(command, {data: options});
-            //命令为exit时终止任务
-            if(command === "exit"){
-                _cb("exit");
-                return;
-            }
-            //命令为skip时跳过任务
-            if(command === "skip"){
-                _cb(null, "skip");
-                return;
-            }
-            
-            if (remote && options[remote]) {
-                logger.info("REMOTE " + remote + ": " + command);
-                var remoteObj = options[remote];
-                var sshConn = sshArray[remote] || new ssh(remoteObj);
-                workPath = remoteObj.workPath;
-                if (sshConn) {
-                    sshArray[remote] = sshConn;
-                    if (workPath) {
-                        command = "cd " + workPath + "; " + command;
-                    }
-                    sshConn.exec(command, _cb);
-                } else {
-                    _cb("ssh object error~!");
-                }
-            } else {
-                logger.info("LOCAL: " + command);
-                localCmd.exec(command, workPath, _cb);
-            }
-        }, function (err, data) {
+        sheller.execTask({
+            "options": options,
+            "task": task
+        }, function (err, data){
             if (typeof err === "string") {
                 logger.error("error: " + err);
             }else if(err){
@@ -103,6 +27,5 @@ module.exports = function (grunt) {
             }
             done();
         });
-
     });
 };
